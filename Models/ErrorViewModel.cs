881e29f025b65a6799d8e5b85590b05426075ad5
shivanimeena125 <shivanimeena7454@gmail.com>
2025-07-17 10:37:11 +0530

    add conyent type

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webAppOrchard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
